29903b334932d01e7d90ba183f46be6d98040a7a
proyecto <pcsijtin@upc.edu.pe>
2023-04-25 08:09:24 -0500

    feat: added behavior for students component.

--- src/app/learning/pages/students/students.component.ts
+++ src/app/learning/pages/students/students.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {Student} from "../../model/student";
+import {MatTableDataSource} from "@angular/material/table";
+import {NgForm} from "@angular/forms";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {StudentsService} from "../../services/students.service";
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit, AfterViewInit {
+  studentData: Student;
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ['id', 'name', 'age', 'address', 'actions'];
+
+  @ViewChild('studentForm', { static: true })
+  studentForm!: NgForm;
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  isEditMode = false;
+
+  constructor(private studentsService: StudentsService) {
+    this.studentData = {} as Student;
+    this.dataSource = new MatTableDataSource<any>();
+  }
+
+  ngAfterViewInit(): void {
+        this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.getAllStudents();
+  }
+
+  getAllStudents() {
+    this.studentsService.getAll().subscribe((response: any) => {
+      this.dataSource.data = response;
+    });
+  }
+
+  editItem(element: Student) {
+    this.studentData = element;
+    this.isEditMode = true;
+  }
+
+  cancelEdit() {
+    this.isEditMode = false;
+    this.studentForm.resetForm();
+  }
+
+  deleteItem(id: number) {
+    this.studentsService.delete(id).subscribe(() => {
+      this.dataSource.data = this.dataSource.data.filter( (o: Student) => {
+        return o.id !== id ? o : false;
+      });
+    });
+    console.log(this.dataSource.data);
+  }
+
+  addStudent() {
+    this.studentsService.create(this.studentData).subscribe((response: any) => {
+      this.dataSource.data.push({...response});
+      this.dataSource.data = this.dataSource.data.map((o: any) => { return o; });
+    });
+  }
+
+  updateStudent() {
+    this.studentsService.update(this.studentData.id, this.studentData).subscribe((response: any) => {
+      this.dataSource.data = this.dataSource.data.map((o: Student) => {
+        if (o.id === response.id) {
+          o = response;
+        }
+        return o;
+      });
+    });
+  }
+
+  onSubmit() {
+    if (this.studentForm.form.valid) {
+      console.log('valid');
+      if (this.isEditMode) {
+        console.log('about to update');
+        this.updateStudent();
+      } else {
+        console.log('about to add');
+        this.addStudent();
+      }
+      this.cancelEdit();
+    } else {
+      console.log('Invalid data');
+    }
+  }
+
 
 }
